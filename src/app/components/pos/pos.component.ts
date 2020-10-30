f7854a1a954c0723f9d6acd5aed17f9f678bf953
DrOotchy <hesham_deraz@live.com>
2020-10-30 02:53:59 +0200

    cashier v1

--- src/app/components/pos/pos.component.ts
+++ src/app/components/pos/pos.component.ts
@@ -14,6 +14,7 @@ export class PosComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMenuCategories()
+    this.getMenuItems(this.menuCategories[0]._id)
   }
 getMenuCategories(){
   return this.menuService.getCategories()
